0d21083dee1eb89d3fbd0ca6f7db090f2dfa14e3
Raul Silva <raul.silva@ua.pt>
2022-11-21 23:16:10 +0000

    Atualização do código da Sala

--- Cinema/src/main/java/DadosPermanentes/Sala.java
+++ Cinema/src/main/java/DadosPermanentes/Sala.java
@@ -1,4 +1,27 @@
 package DadosPermanentes;
 
 public class Sala {
+    private int numero;
+
+    public Sala(int numero){
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) throws Exception {
+        if(numero>0) {
+            this.numero = numero;
+        } else{
+            throw new Exception("O numero da sala tem de ser maior que zero.");
+        }
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(this.numero);
+    }
+
 }
