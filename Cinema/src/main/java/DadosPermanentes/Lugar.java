42104fd7bf0cd5b94fd7ce7011aaafeb9cad11c3
Raul Silva <raul.silva@ua.pt>
2022-11-22 15:31:38 +0000

    Atualização do código do Lugar

--- Cinema/src/main/java/DadosPermanentes/Lugar.java
+++ Cinema/src/main/java/DadosPermanentes/Lugar.java
@@ -1,5 +1,39 @@
 package DadosPermanentes;
 
 public class Lugar {
+    private int numero;
+
+    enum tipo{
+        Deficiente,
+        Normal,
+        VIP
+    }
+    private String[][] posicao;
+    private Sala sala;
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setPosicao(String[][] posicao) {
+        this.posicao = posicao;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+
 
 }
