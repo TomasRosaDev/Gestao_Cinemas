c729ff7e35612a028b3a39895b8c25f3c84e334e
migue <a.fmpsimoes@ua.pt>
2022-12-20 22:18:00 +0000

    Adição de biblioteca para conecção à base de dados

--- Cinema/src/main/java/DadosPermanentes/SbdHandler.java
+++ Cinema/src/main/java/DadosPermanentes/SbdHandler.java
@@ -1,4 +1,56 @@
 package DadosPermanentes;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class SbdHandler {
+    Connection con = null;
+    Statement stmt = null;
+    public SbdHandler() {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://estga-dev.ua.pt/PTDA_BD_04?", "PTDA_4", "Kiut684h");
+            Statement stmt = con.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public ArrayList<Filme> listaFilmes (Calendar dia) {
+        ArrayList<Filme> listaFilmes = new ArrayList<>();
+        ResultSet queryFilmes;//Lista Filmes ativos e corresposndetes ao dia actual ou a um dia passado por parametr
+        try {
+            if (stmt.execute(query)) {
+                queryFilmes = stmt.getResultSet();
+                while (queryFilmes.next()) {
+    
+
+                    listaFilmes.add(new Filme(titulo, tituloOriginal, generos, dataEstreia, realizador,atores,));
+                }
+                return listaFilmes;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /*
+               Statement stmt = conn.createStatement();
+            ResultSet rs;
+            String query = "SELECT nome FROM aluno WHERE numero = " + nAluno;
+
+            if(stmt.execute(query)){
+                rs = stmt.getResultSet();
+
+                while (rs.next()){
+                    String nome = rs.getNString("nome");
+                    System.out.println(nome);
+                }
+
+
+            }
+     */
+
+
 }
